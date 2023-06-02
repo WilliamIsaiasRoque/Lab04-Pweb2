7cb1feffccb81bf461b14b6175812e3c1226b727
William <wroquequi@unsa.edu.pe>
2023-06-02 12:14:49 -0500

    Ejercicio 2a finalizado

--- Ejercicio2a.py
+++ Ejercicio2a.py
@@ -1,9 +1,23 @@
 from interpreter import draw
 from chessPictures import *
 
-caballo = Picture(KNIGHT)
-caballo1=caballo.negative()
-draw(caballo1)
+horse=knight
+horseblack=horse.negative()
+horsewhite=horseblack.negative()
+
+draw(horsewhite.join(horseblack).under(horseblack.join(horsewhite)))
+
+
+"""
+horse = knight
+blackHorse = horse.negative()
+horses = horse.join(blackHorse)
+draw(horses)
+"""
+"""
+horses3 = horse.join(horses)
+horses4 = horse.join(horses3)
+draw(horses4) """
 
 
 
