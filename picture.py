4c943563ddc3adb808768ddaca9304eac6a6f097
William <wroquequi@unsa.edu.pe>
2023-06-02 12:14:28 -0500

    Todos los métodos funcionales

--- picture.py
+++ picture.py
@@ -13,24 +13,24 @@ class Picture:
 
   def verticalMirror(self):
     """ Devuelve el espejo vertical de la imagen """
-    vertical = []
-    for value in self.img:
-    	vertical.append(value[::-1])
-    return vertical
+    vertical = self.img[::-1]
+    return Picture(vertical)
 
   def horizontalMirror(self):
     """ Devuelve el espejo horizontal de la imagen """
-    horizontal=self.img[::-1]
-    return horizontal
-
+    horizontal = []
+    for value in self.img:
+    	horizontal.append(value[::-1])
+    return Picture(horizontal)
+  
   def negative(self):
     """ Devuelve un negativo de la imagen """
-    negative = []
-    for value in self.img:
-      value_n = [self._invColor(color) for color in value]
-      negative.append(value_n)
-    return negative
-
+    neg_img = []
+    for row in self.img:
+      neg_row = [self._invColor(pixel) for pixel in row]
+      neg_img.append(neg_row)
+    return Picture(neg_img)
+    
   def join(self, p):
     """ Devuelve una nueva figura poniendo la figura del argumento 
         al lado derecho de la figura actual """
@@ -42,7 +42,7 @@ class Picture:
     return Picture(joinRslt)
     
   def up(self, p):
-    """ Devuelve una nueva figura poniendo la figura p bajo la figura actual """
+    """ Devuelve una nueva figura poniendo la figura p bajo la figura actuaSl """
     new_img = p.img + self.img
     return Picture(new_img)
 
@@ -55,7 +55,7 @@ class Picture:
   def horizontalRepeat(self, n):
     """ Devuelve una nueva figura repitiendo la figura actual al costado
         la cantidad de veces que indique el valor de n """
-    repeated_img = [pixel for pixel in self.img for _ in range(n)]
+    repeated_img = [row * n for row in self.img]
     return Picture(repeated_img)
 
   def verticalRepeat(self, n):
