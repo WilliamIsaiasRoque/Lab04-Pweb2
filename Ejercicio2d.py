f4ff6a29dca22eb10fa59e31e8ddbf0bb4955f15
William <wroquequi@unsa.edu.pe>
2023-06-05 07:47:05 -0500

    Ejercicio 2d funcional con squares

--- Ejercicio2d.py
+++ Ejercicio2d.py
@@ -1,2 +1,8 @@
 from interpreter import draw
 from chessPictures import *
+
+squarenegative = square.negative() 
+squarewhite = squarenegative.negative()
+fila = squarewhite.join(squarenegative).horizontalRepeat(4)
+
+draw(fila)   
