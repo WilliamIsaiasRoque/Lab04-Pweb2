fa4e2324fedb31dd9b478c3f3487c6ab836ba1d6
William <wroquequi@unsa.edu.pe>
2023-06-05 07:44:42 -0500

    Ejercicio 2b finalizado

--- Ejercicio2b.py
+++ Ejercicio2b.py
@@ -1,2 +1,11 @@
 from interpreter import draw
 from chessPictures import *
+
+horse=knight
+horseblack=horse.negative()
+horsewhite=horseblack.negative()
+horse4mirror=horsewhite.join(horseblack).horizontalMirror().up(horsewhite.join(horseblack))
+
+draw(horse4mirror)
+
+
