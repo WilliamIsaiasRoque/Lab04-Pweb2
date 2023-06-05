ed9d88aeed35cdefeec28e488afb2e491ff2b135
William <wroquequi@unsa.edu.pe>
2023-06-05 07:52:41 -0500

    Ejercicio 2f funcional

--- Ejercicio2f.py
+++ Ejercicio2f.py
@@ -1,2 +1,8 @@
 from interpreter import draw
 from chessPictures import *
+
+squarenegative = square.negative() 
+squarewhite = squarenegative.negative()
+fila = squarewhite.join(squarenegative).horizontalRepeat(4)
+filax4=fila.negative().up(fila).verticalRepeat(2)
+draw(filax4)
