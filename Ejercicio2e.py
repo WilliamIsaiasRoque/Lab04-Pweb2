b5e54c09ed4ac7c610894664ccade10784435bd8
William <wroquequi@unsa.edu.pe>
2023-06-05 07:51:37 -0500

    Ejercicio 2e funcional usando de referencia el ejercicio 2d

--- Ejercicio2e.py
+++ Ejercicio2e.py
@@ -1,2 +1,8 @@
 from interpreter import draw
 from chessPictures import *
+
+squarenegative = square.negative() 
+squarewhite = squarenegative.negative()
+fila = squarenegative.join(squarewhite).horizontalRepeat(4)
+
+draw(fila)
