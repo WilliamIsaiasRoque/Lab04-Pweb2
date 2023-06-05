c952c4f0fce0f69ab86c319702e574e547bd7968
William <wroquequi@unsa.edu.pe>
2023-06-05 07:54:00 -0500

    Probando el ejercicio 2g

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -1,2 +1,19 @@
 from interpreter import draw
 from chessPictures import *
+
+attack = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock)
+pawns = pawn.horizontalRepeat(8)
+squarenegative = square.negative() 
+squarewhite = squarenegative.negative()
+
+fila = squarewhite.join(squarenegative).horizontalRepeat(4)
+filax4=fila.negative().up(fila).verticalRepeat(2)
+
+filax8=fila.negative().up(fila).verticalRepeat(4)
+
+super=square.under(rock)
+draw(super)
+
+
+
+
