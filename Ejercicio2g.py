18d52d915fbb1c46b21cea20718fd40c5385dc33
William <wroquequi@unsa.edu.pe>
2023-06-07 16:28:00 -0500

    Probando impresión de piezas con sus square

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -1,18 +1,32 @@
 from interpreter import draw
 from chessPictures import *
 
-attack = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock)
-pawns = pawn.horizontalRepeat(8)
-squarenegative = square.negative() 
-squarewhite = squarenegative.negative()
+squareN=square.negative()
 
-fila = squarewhite.join(squarenegative).horizontalRepeat(4)
+#Reutilizando la filax4 para imprimir squares vacios para el tablero
+fila = square.join(squareN).horizontalRepeat(4)
 filax4=fila.negative().up(fila).verticalRepeat(2)
 
-filax8=fila.negative().up(fila).verticalRepeat(4)
+#Creando "attack" este contiene el torre,caballo,alfil,rey,reina con su square
+attack = squareN.under(rock)
+attack = attack.join(square.under(knight)) 
+attack = attack.join(squareN.under(bishop))
+attack = attack.join(square.under(queen))
+attack = attack.join(squareN.under(king))
+attack = attack.join(square.under(bishop))
+attack = attack.join(squareN.under(knight))
+attack = attack.join(square.under(rock))
 
-super=square.under(rock)
-draw(super)
+#creando "pawns" este contiene los 8 peones con su respectivo square
+pawns = square.under(pawn)
+i=0
+while(i<7):
+    if i%2==0:
+        pawns = pawns.join(squareN.under(pawn))
+    else:
+        pawns = pawns.join(square.under(pawn))
+    i+=1    
+draw(pawns)
 
 
 
